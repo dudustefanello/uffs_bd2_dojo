d2f71405c28966a659c646e7327f975a26299ab3
Eduardo Stefanello <eduardo@stefanello.cc>
2022-09-20 21:48:55 -0300

    11

--- responses.sql
+++ responses.sql
@@ -1,11 +1,11 @@
-/* 1 */
+/* 1. Listar os empregados (nomes) que tem salário maior que seu chefe */
 select *
 from empregados as emp
 join empregados as chf
     on chf.emp_id = emp.supervisor_id
 where chf.salario < emp.salario;
 
-/* 2 */
+/* 2. Listar o maior salario de cada departamento (pode ser usado o group by) */
 select dep.nome,
     max(emp.salario)
 from empregados as emp
@@ -13,7 +13,7 @@ join departamentos as dep
     on dep.dep_id = emp.dep_id
 group by dep.nome;
 
-/* 3 */
+/* 3. Listar o nome do funcionario com maior salario dentro de cada departamento (pode ser usado o IN) */
 with max_dep as (
     select dep.dep_id,
         dep.nome,
@@ -29,7 +29,7 @@ join max_dep
     on emp.dep_id = max_dep.dep_id
     and emp.salario = max_dep.avg;
 
-/* 4 */
+/* 4. Listar os nomes departamentos que tem menos de 3 empregados; */
 select dep.nome,
     count(*)
 from empregados as emp
@@ -38,7 +38,9 @@ join departamentos as dep
 group by dep.nome
 having count(*) < 3;
 
-/* 5 */
+/* NÃO TEM 5 */
+
+/* 6. Listar os departamentos  com o número de colaboradores */
 select dep.nome,
     count(*)
 from empregados as emp
@@ -47,14 +49,14 @@ join departamentos as dep
 group by dep.nome
 order by count desc;
 
-/* 6 */
+/* 7. Listar os empregados que não possuem chefes no mesmo departamento */
 select *
 from empregados as emp
 join empregados as chf
     on chf.emp_id = emp.supervisor_id
 where chf.dep_id <> emp.dep_id;
 
-/* 7 */
+/* 8. Listar os departamentos com o total de salários pagos */
 select dep.nome,
     sum(salario)
 from empregados as emp
@@ -62,7 +64,7 @@ join departamentos as dep
     on dep.dep_id = emp.dep_id
 group by dep.nome;
 
-/* 8 */
+/* 9. Listar os colaboradores com salario maior que a média do seu departamento; */
 with med_dep as (
     select dep.dep_id,
         dep.nome,
@@ -77,3 +79,32 @@ from empregados as emp
 join med_dep
     on emp.dep_id = med_dep.dep_id
     and emp.salario > med_dep.avg;
+
+/* 10. Compare o salario de cada colaborados com média do seu setor. Dica: usar slide windows functions */
+select emp.nome,
+    emp.salario,
+    dep.nome,
+    avg(emp.salario) over (partition by dep.dep_id)
+from empregados as emp
+join departamentos as dep
+    on dep.dep_id = emp.dep_id
+group by emp.nome, 
+    emp.salario,
+    dep.dep_id;
+
+/* 11. Encontre os empregados com salario maior ou igual a média do seu departamento. 
+       Deve ser reportado o salario do empregado e a média do departamento */
+with med_dep as (
+    select dep.dep_id,
+        dep.nome,
+        avg(emp.salario)
+    from empregados as emp
+    join departamentos as dep
+        on dep.dep_id = emp.dep_id
+    group by dep.dep_id
+)
+select *
+from empregados as emp
+join med_dep
+    on emp.dep_id = med_dep.dep_id
+    and emp.salario >= med_dep.avg;
