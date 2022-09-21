187f3193e38181975e3c151bc2df322b2c827a55
Eduardo Stefanello <eduardo@stefanello.cc>
2022-09-20 21:59:41 -0300

    livre

--- responses.sql
+++ responses.sql
@@ -1,5 +1,8 @@
 /* 1. Listar os empregados (nomes) que tem salário maior que seu chefe */
-select *
+select emp.nome as empregado,
+    emp.salario as salario_empr,
+    chf.nome as chefe,
+    chf.salario as salario_chef
 from empregados as emp
 join empregados as chf
     on chf.emp_id = emp.supervisor_id
@@ -7,11 +10,12 @@ where chf.salario < emp.salario;
 
 /* 2. Listar o maior salario de cada departamento (pode ser usado o group by) */
 select dep.nome,
-    max(emp.salario)
+    max(emp.salario) as maior_salario
 from empregados as emp
 join departamentos as dep
     on dep.dep_id = emp.dep_id
-group by dep.nome;
+group by dep.nome
+order by maior_salario desc;
 
 /* 3. Listar o nome do funcionario com maior salario dentro de cada departamento (pode ser usado o IN) */
 with max_dep as (
@@ -23,15 +27,14 @@ with max_dep as (
         on dep.dep_id = emp.dep_id
     group by dep.dep_id
 )
-select *
+select emp.nome, emp.salario
 from empregados as emp
 join max_dep
     on emp.dep_id = max_dep.dep_id
-    and emp.salario = max_dep.avg;
+    and emp.salario = max_dep.max;
 
 /* 4. Listar os nomes departamentos que tem menos de 3 empregados; */
-select dep.nome,
-    count(*)
+select dep.nome
 from empregados as emp
 join departamentos as dep
     on dep.dep_id = emp.dep_id
@@ -42,12 +45,12 @@ having count(*) < 3;
 
 /* 6. Listar os departamentos  com o número de colaboradores */
 select dep.nome,
-    count(*)
+    count(*) as numero_colabs
 from empregados as emp
 join departamentos as dep
     on dep.dep_id = emp.dep_id
 group by dep.nome
-order by count desc;
+order by numero_colabs desc;
 
 /* 7. Listar os empregados que não possuem chefes no mesmo departamento */
 select *
@@ -58,11 +61,12 @@ where chf.dep_id <> emp.dep_id;
 
 /* 8. Listar os departamentos com o total de salários pagos */
 select dep.nome,
-    sum(salario)
+    sum(salario) as total
 from empregados as emp
 join departamentos as dep
     on dep.dep_id = emp.dep_id
-group by dep.nome;
+group by dep.nome
+order by dep.nome;
 
 /* 9. Listar os colaboradores com salario maior que a média do seu departamento; */
 with med_dep as (
@@ -108,3 +112,14 @@ from empregados as emp
 join med_dep
     on emp.dep_id = med_dep.dep_id
     and emp.salario >= med_dep.avg;
+
+/* QUESTÃO LIVRE:
+   Mostrar o total de salários dos supordinados de cada chefe  */
+select chf.nome as nome_chefe,
+    sum(emp.salario) total_salarios
+from empregados as emp
+join empregados as chf
+    on chf.emp_id = emp.supervisor_id
+group by 1
+order by 2 desc;
+
